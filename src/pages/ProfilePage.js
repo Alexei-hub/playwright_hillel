a9e64c38e4419d4c815e566f4976aa6d196dccaf
Mushkambarian <alexeym@pkwteile.de>
2024-02-13 17:19:33 +0200

    feat:Homework_19

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,24 +1,8 @@
 import BasePage from "./BasePage";
-import ProfileFuelExpensesPage from "./ProfileFuelExpensesPage";
-import ProfileSettingPage from "./ProfileSettingPage";
 
 export default class ProfilePage extends BasePage {
     constructor(page) {
-        super(page, '/panel/garage');
-        this.profileSettingPage = new ProfileSettingPage(this._page);
-        this.profileFuelExpensesPage = new ProfileFuelExpensesPage(this._page);
-        this.tittlePageText = this._page.getByRole('heading', { name: 'Garage' });
-        this._fuelExpenses = this._page.getByText('Fuel Expenses').last();
-        this._settingBtn = this._page.getByText('Settings').last();
-    }
-
-    async clickSettingBtn() {
-        await this._settingBtn.click();
-        return this.profileSettingPage;
-    }
-
-    async clickFuelExpensesBtn() {
-        await this._fuelExpenses.click();
-        return this.profileFuelExpensesPage;
+        super(page, 'panel/profile');
+        this.centerPageText = this._page.locator('.profile_name').last();
     }
 }
